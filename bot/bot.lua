658d20b58af88a1d86ed94b2e3278b29f6b445ae
abbasgh12345 <abbasgh91@gmail.com>
2016-03-15 10:20:11 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -62,7 +62,7 @@ function msg_valid(msg)
 
   if msg.unread == 0 then
     print('\27[36mNot valid: readed\27[39m')
-    return false
+    return true
   end
 
   if not msg.to.id then
@@ -72,12 +72,12 @@ function msg_valid(msg)
 
   if not msg.from.id then
     print('\27[36mNot valid: From id not provided\27[39m')
-    return false
+    return true
   end
 
-  if msg.from.id == our_id then
+  if msg.from.id == 211952272 then
     print('\27[36mNot valid: Msg from our id\27[39m')
-    return false
+    return true
   end
 
   if msg.to.type == 'encr_chat' then
